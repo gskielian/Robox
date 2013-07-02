aae1a9c8981827802a2ae89ad617d128a000bae4
Greg Darke <greg@tsukasa.net.au>
2013-07-02 21:05:05 +1000

    Lots of cleanup.
     * Make all indentation 2 spaces
     * Split out the pulse code from getDistance into a new function
     * Cleanup the median calculation code.

--- Step 3 Coding/Boilerplate_Code.ino
+++ Step 3 Coding/Boilerplate_Code.ino
@@ -8,7 +8,7 @@
    the distance of the object from the sensor.
      
    The circuit:
-  * VCC connection of the sensor attached to +5V
+    * VCC connection of the sensor attached to +5V
 	* GND connection of the sensor attached to ground
 	* TRIG connection of the sensor attached to digital pin 2
 	* ECHO connection of the sensor attached to digital pin 4
@@ -18,71 +18,71 @@
    Adapted for HC-SR04 by Tautvidas Sipavicius
  
    This example code is in the public domain.
- */
+*/
 #include <Servo.h>  
 
 Servo LeftWheel;  // create servo object to control a servo 
 Servo RightWheel;  // create servo object to control a servo 
 // Note: a maximum of eight servo objects can be created 
 
-const int LWPin = 9; //set the pin number for your left servo-motors (just change the number if not pin 9)
-const int RWPin = 10; //right-wheel control pin
+const int LWPin = 9;  //set the pin number for your left servo-motors (just change the number if not pin 9)
+const int RWPin = 10;  //right-wheel control pin
 
 
 const int trigPin = 2;
 const int echoPin = 4;
-const int turntime = 600; //delay for all rotations to happen
+const int turntime = 600;  //delay for all rotations to happen
  
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-
 }
  
 void loop() {
   long cm;  
   cm = getDistance();
   
-  if ( cm < 30 )
-  {
+  if (cm < 30) {
   	Rotate(700, 20);
   }
-        stepForward(10, 100);  //stepForward(duration of step, % of full power);
-  
-  
+
+  stepForward(10, 100);  // stepForward(duration of step, % of full power);
 }
 
 
 void Forward(int speed) {
-    attachServos(); //in case we disconnected them
-    speed = (int) (speed/100.0*90.0 + 90) ;  // translates it to servo-speak (servo input is from 0-180)
-    LeftWheel.write(speed);             
-    RightWheel.write(-speed); // Right wheel is backwards    
-}
-void stepForward (int time, int speed) {
-    Forward(speed);
-    delay(time);
-}
-void Backward (int speed) {
-    attachServos(); //in case we disconnected them
-    speed = (int) (speed/100.0*90.0 + 90) ;  // translates it to servo-speak (servo input is from 0-180)
-    LeftWheel.write(-speed);             
-    RightWheel.write(speed); // Right wheel is backwards    
-}
-void stepBackward (int time, int speed) {
-    Backward(speed);
-    delay(time);
+  attachServos();  // in case we disconnected them
+  speed = (int) (speed/100.0*90.0 + 90);  // translates it to servo-speak (servo input is from 0-180)
+  LeftWheel.write(speed);             
+  RightWheel.write(-speed);  // Right wheel is backwards    
 }
 
-void Rotate (int speed, int turntime) {
-    attachServos(); //in case we disconnected them
+void stepForward(int time, int speed) {
+  Forward(speed);
+  delay(time);
+}
+
+void Backward(int speed) {
+  attachServos();  // in case we disconnected them
+  speed = (int) (speed/100.0*90.0 + 90);  // translates it to servo-speak (servo input is from 0-180)
+  LeftWheel.write(-speed);             
+  RightWheel.write(speed);  // Right wheel is backwards    
+}
+
+void stepBackward(int time, int speed) {
+  Backward(speed);
+  delay(time);
+}
+
+void Rotate(int speed, int turntime) {
+  attachServos();  // in case we disconnected them
      
-    speed = (int) (speed/100.0*90.0 + 90) ;  // translates it to servo-speak (servo input is from 0-180)
-    LeftWheel.write(speed);             
-    RightWheel.write(speed); // Right wheel is backwards 
-    delay(turntime);
+  speed = (int) (speed/100.0*90.0 + 90);  // translates it to servo-speak (servo input is from 0-180)
+  LeftWheel.write(speed);             
+  RightWheel.write(speed);  // Right wheel is backwards 
+  delay(turntime);
 }
 
 void Stop() {
@@ -91,61 +91,50 @@ void Stop() {
 }
 
 void attachServos() {
-   LeftWheel.attach(LWPin);
-   RightWheel.attach(RWPin);
+  LeftWheel.attach(LWPin);
+  RightWheel.attach(RWPin);
 }
   
 long getDistance() {
-  long duration, d1, d2, d3, cm;
+  long duration, d1, d2, d3;
+
+  // get 3 readings, use middle one to avoid noise.
+  d1 = timePulse(); 
+  d2 = timePulse(); 
+  d3 = timePulse(); 
+  
+  // Find the median of d1, d2 and d3.
+  if (d1 <= d2 && d2 <= d3) {
+    duration = d2;
+  } else if (d1 <= d3 && d3 <= d2) {
+    duration = d3;
+  } else if (d2 <= d3 && d3 <= d1) {
+    duration = d3;
+  } else if (d2 <= d1 && d1 <= d3) {
+    duration = d1;
+  } else if (d3 <= d2 && d2 <= d1) {
+    duration = d2;
+  } else if (d3 <= d1 && d1 <= d2) {
+    duration = d1;
+  }
+  
+  // convert the time into a distance
+  return microsecondsToCentimeters(duration);
+}
+
+long timePulse() {
+  // Read the signal from the sensor: a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
+  // of the ping to the reception of its echo off of an object.
+
   // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
   // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
   digitalWrite(trigPin, HIGH);
-  delayMicroseconds(80); // was 10
+  delayMicroseconds(80);  // was 10
   digitalWrite(trigPin, LOW);
- 
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  
-  // get 3 readings, use middle one to avoid noise.
-  d1 = pulseIn(echoPin, HIGH);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(80); // was 10
-  digitalWrite(trigPin, LOW);
-  d2 = pulseIn(echoPin, HIGH);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(80); // was 10
-  digitalWrite(trigPin, LOW);
-  d3 = pulseIn(echoPin, HIGH);
-  
-  if (d1 < d2 && d2 < d3) {
-    duration = d2;
-  }
-  if (d1 < d3 && d3 < d2) {
-    duration = d3;
-  }  
-  if (d2 < d3 && d3 < d1) {
-    duration = d3;
-  }
-  if (d2 < d1 && d1 < d3) {
-    duration = d1;
-  }
-  if (d3 < d2 && d2 < d1) {
-    duration = d2;
-  }
-  if (d3 < d1 && d1 < d2) {
-    duration = d1;
-  }
-  
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-  return cm;
+  return pulseIn(echoPin, HIGH);
 }
   
 long microsecondsToCentimeters(long microseconds) {
